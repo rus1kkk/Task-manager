de70158bb8da2d5d4f02dd43b82b5882d1ed3576
killer_rusik <ruslan.pivnik@yandex.ru>
2025-07-13 20:21:17 +0300

    Commit for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
+  base: '/Task-manager/',
   plugins: [react()],
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
